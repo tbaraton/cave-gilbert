d063f556fc31f0afaecb9a6ebb6c2ebcd4f287e8
tbaraton <tbaraton@gmail.com>
2026-05-12 14:21:23 +0200

    commit

--- src/app/admin/location/page.tsx
+++ src/app/admin/location/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { ModuleRetourLocation } from '../retour-location'
+import { ModuleRetourLocation } from '../../caisse/retour-location'
 
 import { useState, useEffect, useCallback } from 'react'
 import { createClient } from '@supabase/supabase-js'
